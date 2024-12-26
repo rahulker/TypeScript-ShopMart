91cb5126b1132569f63abad204df0bb57e55808c
rahulker1 <mt_0226@outlook.com>
2024-12-26 14:29:38 +0530

    user profile

--- src/pages/userProfile/page.tsx
+++ src/pages/userProfile/page.tsx
@@ -2,6 +2,7 @@ import { useSelector } from "react-redux";
 import { rootState } from "../../store/store";
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 
 const Page = () => {
   const userData = useSelector((state: rootState) => state.user.userDetail);
@@ -16,7 +17,21 @@ const Page = () => {
     }
   });
 
-  return <div>{userData.name}</div>;
+  return (
+    <div className="w-full gap-8 flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center">
+        <RxAvatar size={50} />
+        <div className="mt-2.5">
+          <h2 className="text-2xl font-bold">Welcome, {userData.name}</h2>
+        </div>
+      </div>
+      <div className="grid grid-cols-2 items-center justify-between w-2/3">
+        <div>1</div>
+        <div>2</div>
+        <div>3 </div>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
