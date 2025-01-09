2d0ac0a17a08549bf402644d2125afd3c951a08d
rahulker1 <mt_0226@outlook.com>
2025-01-09 14:13:24 +0530

    folder changes

--- src/Components/exports.ts
+++ src/Components/exports.ts
@@ -10,5 +10,5 @@ export { default as InputAndLabel } from "./common/inputsAndLabel/page";
 export { default as InputAndText } from "./common/inputAndText/page";
 export { default as DeleteModel } from "./common/model/page";
 export { default as BuyNowCard } from "./common/buyNowCard/page";
-export {default as SearchModel } from "./searchModal/component"
-export {default as SearchItem } from "./searchItem/component"
+export {default as SearchModel } from "./common/searchModal/component"
+export {default as SearchItem } from "./common/searchItem/component"
